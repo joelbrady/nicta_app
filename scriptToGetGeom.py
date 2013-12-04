fa8d76816aae65a2bb16379d95ea734c6574d21b
Lian <hljw4@hotmail.com>
2013-12-04 03:04:04 -0800

    New coor datas

--- scriptToGetGeom.py
+++ scriptToGetGeom.py
@@ -1,8 +1,11 @@
 import urllib2,json
 
 data=json.load(urllib2.urlopen("http://envirohack.research.nicta.com.au/fsdf-topology/units/?unitType=STATE"))
-for i in data:
-	print i["simplifiedGeometryURL"]
-	coor=json.load(urllib2.urlopen(i["simplifiedGeometryURL"]))
-	print coor["features"][0]["geometry"]["coordinates"]
-	break
+for i in xrange(len(data)):
+	#print i["simplifiedGeometryURL"]
+	coor=json.load(urllib2.urlopen(data[i]["simplifiedGeometryURL"]))
+	#print coor["features"][0]["geometry"]["coordinates"]
+	f=open('myfile'+str(i),'w')
+	f.write(str(coor["features"][0]["geometry"]["coordinates"][0][0]))
+	f.close
+	
